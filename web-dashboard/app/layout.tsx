261f464aebbd675ad2c908acb530bc368940905a
Duc Hoang Xuan <hhoangducz1@gmail.com>
2026-09-10 17:29:33 +0700

    Tích hợp DashboardShell và đồng bộ layout giao diện tổng thể

--- web-dashboard/app/layout.tsx
+++ web-dashboard/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Link from 'next/link';
 import React from 'react';
-import Navigation from '../components/Navigation';
+import DashboardShell from '../components/DashboardShell';
 import { LanguageProvider } from '../components/LanguageContext';
 import { ThemeProvider } from '../components/ThemeContext';
 import { TabProvider } from '../components/TabContext';
 import { CameraProvider } from '../components/CameraContext';
-import HeaderActions from '../components/HeaderActions';
-import ChatbotWidget from '../components/ChatbotWidget';
 
 export const metadata: Metadata = {
   title: 'VMS-RTC',
@@ -27,31 +24,7 @@ export default function RootLayout({
           <LanguageProvider>
             <CameraProvider>
               <TabProvider>
-                <div className="fms-layout">
-              {/* TOP HEADER */}
-              <header className="fms-header">
-                {/* Left: Brand */}
-                <div className="fms-brand">
-                  <img src="/logo.png" alt="Header Logo" style={{ height: '64px', objectFit: 'contain' }} />
-                  <span className="brand-text" style={{ marginLeft: '4px' }}>VMS</span>
-                  <span className="brand-version">v 1.0</span>
-                </div>
-
-                {/* Center: Navigation */}
-                <Navigation />
-
-                {/* Right: Actions */}
-                <HeaderActions />
-              </header>
-
-              {/* MAIN CONTENT AREA */}
-              <main className="fms-main">
-                {children}
-              </main>
-
-              {/* CHATBOT WIDGET */}
-              <ChatbotWidget />
-              </div>
+                <DashboardShell>{children}</DashboardShell>
             </TabProvider>
           </CameraProvider>
         </LanguageProvider>
